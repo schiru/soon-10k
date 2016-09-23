4a7086164557aa4f02cb9894cf12ea588757314a
schiru <jirout.thomas@gmail.com>
2016-09-23 04:48:20 +0200

    Added basic SQLite3 database integration (table create, retrieve detail for given id)

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./soon.db');
+
+db.serialize(() => {
+	db.run(`CREATE TABLE IF NOT EXISTS 'countdown' (
+		'id'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+		'title'	TEXT NOT NULL,
+		'description'	TEXT,
+		'startTimestamp'	INTEGER,
+		'endTimestamp'	INTEGER NOT NULL,
+		'createdTimestamp'	INTEGER NOT NULL,
+		'deletePassphrase'	TEXT
+	)`);
+
+	db.run(`CREATE TABLE IF NOT EXISTS 'hashtag' (
+		'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+		'name' TEXT NOT NULL UNIQUE
+	)`);
+
+	db.run(`CREATE TABLE IF NOT EXISTS 'hashtagAssociation' (
+		'hashtagId'	INTEGER NOT NULL,
+		'countdownId'	INTEGER NOT NULL,
+		PRIMARY KEY('hashtagId','countdownId'),
+		FOREIGN KEY('hashtagId') REFERENCES hashtag(id),
+		FOREIGN KEY('countdownId') REFERENCES countdown(id)
+	)`);
+});
 
 var app = express();
 
@@ -18,10 +45,65 @@ app.get('/create', function (req, res) {
 		});
 });
 
-app.get('/c/*', function (req, res) {
-    res.render('detail');
+app.get('/c/:id', function (req, res) {
+		let id;
+		if (req.params.id && !isNaN(id = parseInt(req.params.id))) {
+			console.log(`detail view for id ${id} requested...`);
+
+			db.all(`SELECT cd.id, cd.title, cd.description, cd.endTimestamp
+				FROM countdown cd
+				WHERE cd.id = ?`, [id], (err, infos) => {
+
+					if (!err && infos.length === 1) {
+						let info = infos[0];
+						let hashtags = [];
+
+						db.each(`SELECT ht.name
+							FROM hashtagAssociation hA
+							JOIN hashtag ht ON hA.hashtagId = ht.id
+							WHERE hA.countdownId = ?`, [id], (err, hashtag) => {
+
+								if (!err) {
+									console.log(hashtag);
+									hashtags.push(hashtag.name);
+								} else
+									throw err;
+
+							}, () => {
+								console.log('import done', info);
+								res.render('detail', {
+									cTitle: info.title,
+									cDescription: info.description
+								});
+							});
+					} else {
+						throw err;
+					}
+				});
+
+
+
+		} else {
+			res.status(400);
+			res.end();
+		};
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+function cleanup() {
+	console.log('Shutting down...');
+	db.close();
+}
+
+process.on('exit', (code) => {
+	cleanup();
+	process.exit(code);
+});
+
+process.on('SIGINT', (code) => {
+	cleanup();
+	process.exit(code);
 });
