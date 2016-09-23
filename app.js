265277d0279030b242aa1d2587bab01118d178f7
schiru <jirout.thomas@gmail.com>
2016-09-23 02:00:19 +0200

    Basic routing and handlebars structure introduced

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get(['/', 'index'], function (req, res) {
+    res.render('index', {
+			title: "Soon"
+		});
+});
+
+app.get('/create', function (req, res) {
+    res.render('create', {
+			title: "Create Countdown - Soon"
+		});
+});
+
+app.get('/c/*', function (req, res) {
+    res.render('detail');
 });
 
 app.listen(3000, function () {
