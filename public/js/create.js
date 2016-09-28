4856f77ad7dee1b00a3c1ad1141390bc5a92d8ec
wolfgang Rumpler <contact@iflow.space>
2016-09-28 03:25:31 +0200

    implement automatic timezone detection with clientside js

---
+++ public/js/create.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log('Ready for takeoff');
+
+  var timeInp = document.querySelector('#time');
+  timeInp.addEventListener('keyup', updateTzValue);
+
+  var dateInp = document.querySelector('#date');
+  dateInp.addEventListener('keyup', updateTzValue);
+
+  var tzLabel = document.querySelector('#l_tz');
+  var tzInp = tzLabel.querySelector('input');
+  tzLabel.style.display = "none";
+  updateTzValue();
+
+  function getTzOff(date) {
+    return (date.getTimezoneOffset() * -1)/60;;
+  }
+
+  function updateTzValue() {
+    var tmpDate = new Date(getDateTimeString(dateInp.value, timeInp.value));
+    if (!isNaN(tmpDate.getTime())) {
+      tzInp.value = getTzOff(tmpDate);
+    } else {
+      tzInp.value = getTzOff(new Date());
+    }
+  }
+
+  function getDateTimeString(dateString, timeString) {
+  	if (timeString != '') {
+  		return dateString+'T'+timeString;
+  	} else {
+      return dateString
+    }
+  }
+
+});
